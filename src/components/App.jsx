ab2215a82fe7030825b6f7c41b848351586a4763
Jose <jomaamga@outlook.es>
2025-03-02 04:52:48 +0100

    using config provider is now available

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,51 @@
-import {CARDS} from "../common/constants.js";
+import { useConfig } from "../contexts/ConfigContext.jsx";
 import Card from "./Card.jsx";
 import ThemeButton from "./ThemeButton.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+    const {config, loading, error} = useConfig();
 
-  return (
-    <>
-        <div className={"container py-5"}>
-            <h1 className={"text-center mb-5"}>
-                <a href="https://miarma.net/">miarma.net</a> hub
-            </h1>
-            <div className={"row g-4"}>
-                {CARDS.map((card, index) => (
-                    <Card key={index} {...card} />
-                ))}
+    if (loading) {
+        return (
+            <div className={"text-center py-5"}>
+                <FontAwesomeIcon icon={faSpinner} size={"6x"} spin={true} />
             </div>
-        </div>
+        )
+    }
 
-        <footer>
-            <p>&copy; 2025 miarma.net</p>
-        </footer>
+    if (error) {
+        return (
+            <div className={"text-center py-5"}>
+                <h1>Error</h1>
+                <p>{error.message}</p>
+            </div>
+        )
+    }
 
-        <ThemeButton />
-    </>
-  )
+    return (
+        <>
+            <div className={"container py-5"}>
+                <h1 className={"text-center mb-5"}>
+                    <a href="https://miarma.net/">miarma.net</a> hub
+                </h1>
+                <div className={"row g-4"}>
+                    {config.map((card, index) => (
+                        <Card key={index} {...card} />
+                    ))}
+                </div>
+            </div>
+
+            <footer>
+                <p>&copy; 2025 miarma.net</p>
+            </footer>
+
+            <ThemeButton />
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
+import { ConfigProvider } from './contexts/ConfigContext.jsx'
 import './css/index.css';
 import './css/Card.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
 )
